c153fdff79ed0146d159019fda0bf2b212a9be68
alexvsl2007 <alexvsl2007@gmail.com>
2025-03-17 15:21:29 +0300

    Updated Hello World program to ask for user's name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, world from " << name << "!" << endl;
+    return 0;
+}
+#include <iostream>
 
 using namespace std;
 
