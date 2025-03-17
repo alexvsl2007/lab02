dedec408fed679cb695a574d656a7d06dffaef9b
alexvsl2007 <alexvsl2007@gmail.com>
2025-03-17 15:58:04 +0300

    Formatted code with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::string name;
-    std::cout << "Enter your name: "; // Prompt user for name
-    std::cin >> name;
-    std::cout << "Hello, world from " << name << "!" << std::endl; // Greet user
-    return 0;
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: "; // Prompt user for name
+  std::cin >> name;
+  std::cout << "Hello, world from " << name << "!" << std::endl; // Greet user
+  return 0;
 }
