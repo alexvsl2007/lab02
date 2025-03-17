0a3a1f6aa938ec9b3216460c296453a00e8c68e6
alexvsl2007 <alexvsl2007@gmail.com>
2025-03-17 15:39:12 +0300

    Added comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,8 @@
 
 int main() {
     std::string name;
-    std::cout << "Enter your name: ";
+    std::cout << "Enter your name: "; // Prompt user for name
     std::cin >> name;
-    std::cout << "Hello, world from " << name << "!" << std::endl;
+    std::cout << "Hello, world from " << name << "!" << std::endl; // Greet user
     return 0;
 }
-
