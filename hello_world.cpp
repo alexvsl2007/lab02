e2f8ea171dfc7c1496a29575ac6b5c04f3ec10d4
alexvsl2007 <alexvsl2007@gmail.com>
2025-03-19 10:30:02 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main() {
-  std::string name;
-  std::cout << "Enter your name: "; // Request user name
-  std::cin >> name;
-  std::cout << "Hello, World! from " << name << std::endl; // Output greeting
-  return 0;
+    std::string name;
+    std::cout << "Введите ваше имя: "; // Запрос имени пользователя
+    std::cin >> name;
+    std::cout << "Привет, мир! от " << name << std::endl; // Вывод приветствия
+    return 0;
 }
